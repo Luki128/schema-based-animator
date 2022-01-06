6533d9bfe5731dae338e4fcac7e215d9b5474b0c
leonhardk <konrad.leonhard@snpgroup.com>
2022-01-06 17:20:53 +0100

    Init interfaces

---
+++ schema-based-animator/Command/CommandSequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace schema_based_animator.Command
+{
+    class CommandSequence<T> where T : ITransform
+    {
+        List<Command<T>> commands = new List<Command<T>>();
+        T getTransformAtFrame(int frame)
+        {
+            int i = 0;
+            for (; i < commands.Count && commands[i].frame <= frame; i++) ;
+            return (T)commands[i - 1].value.interpolate((float)(frame-commands[i - 1].frame)/(commands[i].frame-commands[i - 1].frame), commands[i].value);
+        }
+
+        void addCommand(Command<T> command)
+        {
+            commands.Add(command);
+            commands.Sort(Comparer<Command<T>>.Default);
+        }
+    }
+}
