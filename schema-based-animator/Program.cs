0f3ede3fe3a4f40ab08480f6be08c002f0d5a9c7
lukasz L <fineazs@interia.pl>
2022-01-06 17:25:48 +0100

    #2

--- schema-based-animator/Program.cs
+++ schema-based-animator/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Linq;
+using FFMpegCore;
+using LewyDiagnostic;
+
 
 namespace schema_based_animator
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dbg.Title("test", 60,'-', ConsoleColor.Blue);
+
+            //   var t = FFmpeg.Conversions.New();
+           
+            FFMpeg.JoinImageSequence(@"joined_video.mp4", frameRate: 1,
+                ImageInfo.FromPath(@"1.png"),
+                ImageInfo.FromPath(@"2.png"),
+                ImageInfo.FromPath(@"3.png")          
+            );
+
+            dbg.Title("done", 60, '-', ConsoleColor.Blue);
+
+           
+            //  t.SetFrameRate(2);
+            //   var strem = Cr
+
         }
+
     }
 }
