ff7bc972c2379755ad965a3423026b51b82ac92b
Luki128 <54844692+Luki128@users.noreply.github.com>
2022-01-11 10:20:38 +0100

    small finish

--- schema-based-animator/Program.cs
+++ schema-based-animator/Program.cs
@@ -8,21 +8,21 @@ namespace schema_based_animator
 {
     class Program
     {     
-
+        
         static void Main(string[] args)
         {
+            string scName = "test.txt";
+            if(args.length > 0)scName = args[0];
             while (true)
             {
                 dbg.Info("Rendering...");
                 Interpereter interpereter = new Interpereter();
-                interpereter.RunScript("test.txt");
+                interpereter.RunScript(scName);
                 dbg.Succes("Done.");
                 GC.Collect();
                 Console.ReadKey();
                
-
             }
-          //  interpereter.video("test.mp4");
         }
 
     }
